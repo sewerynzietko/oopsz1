2fd4b95d72be4b47c7a0b457564a0bec6ee87898
Seweryn <sewezietko@gmail.com>
2025-04-07 15:59:50 +0200

    lab6

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hawo world!");
+
+        List<Person> people = new ArrayList<>();
+        Person parent = new Person("Alojzy", "Czeremcha", LocalDate.of(1903,12,7));
+        Person child1 = new Person("Grażyna", "Duda", LocalDate.of(1951,4,7));
+        Person child2 = new Person("Ksawery", "Czeremcha", LocalDate.of(1955,8,12));
+        Person child3 = new Person("Brajan", "Czeremcha", LocalDate.of(1959,2,18));
+        Person child4 = new Person("Dżesika", "Mercedes", LocalDate.of(2000,7,11));
+        people.add(parent);
+        people.add(child1);
+        people.add(child2);
+        people.add(child3);
+        people.add(child4);
+        parent.adopt(child1);
+        parent.adopt(child2);
+        parent.adopt(child3);
+        parent.adopt(child4);
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,44 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Person implements Comparable<Person>{
+    private String fname, lname;
+    private LocalDate birthDate;
+    private Set<Person> children;
+    public boolean adopt(Person child){
+        return children.add(child);
+    }
+
+    public Person(String fname, String lname, LocalDate birthDate) {
+        this.fname = fname;
+        this.lname = lname;
+        this.birthDate = birthDate;
+        this.children = new TreeSet<>();
+    }
+
+    public Person getYoungestChild(){
+//        if(children.isEmpty())
+//            return null;
+        LocalDate youngestChildAge = LocalDate.MIN;
+        Person youngestChild = null;
+        for (Person child : children){
+            if(child.birthDate.isAfter(youngestChildAge)) {
+                youngestChildAge = child.birthDate;
+                youngestChild = child;
+            }
+        }
+        return youngestChild;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.birthDate.compareTo(o.birthDate);
+    }
+
+    public List<Person> getChildren()
+    {
+        return List.copyOf(children);
+    }
+}
