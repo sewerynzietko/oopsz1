2b3c7fbb8ce78603da69947fb1bf2a33ee7a22d4
Seweryn <sewezietko@gmail.com>
2025-05-05 16:02:49 +0200

    lab9

---
+++ src/CustomList.java
@@ -0,0 +1,89 @@
+import java.util.AbstractList;
+
+public class CustomList<T>{
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size = 0;
+
+    private static class Node<T>{
+        T value;
+        Node<T> next;
+
+        Node(T value){
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public void addLast(T value){
+        Node<T> newNode = new Node<>(value);
+        if(head == null) {
+            head = tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T getLast(){
+        if(head != null)
+            return tail.value;
+        else
+            return null;
+    }
+
+    public T getFirst(){
+        if(head != null)
+            return head.value;
+        else
+            return null;
+    }
+
+    public void addFirst(T value){
+        Node<T> newNode = new Node<>(value);
+        if(head == null) {
+            head = tail = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public T removeFirst(){
+        if(head != null) {
+            T valueToReturn = head.value;
+            head = head.next;
+            if(head == null)
+                tail = null;
+            size--;
+            return valueToReturn;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public T removeLast(){
+        if(tail != null) {
+            T valueToReturn = tail.value;
+            if(head == tail){
+                head = tail = null;
+            }
+            Node<T> currentElement = head;
+            while(currentElement.next != tail)
+                currentElement = currentElement.next;
+            tail = currentElement;
+            tail.next = null;
+            size--;
+            return valueToReturn;
+        }
+        else {
+            return null;
+        }
+    }
+}
